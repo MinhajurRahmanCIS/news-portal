56a7b2685b64268763b94c2417865d5ca5377349
RkzNew <rahamannovel@gmail.com>
2022-09-03 19:21:53 +0600

    card done

--- js/news.js
+++ js/news.js
@@ -9,25 +9,66 @@ const loadCategories = async() => {
 
 const displayCategories = categories => {
 
+
+
     // console.log(categories);
     const categoriesContainer = document.getElementById("categories-container");
 
     categories.forEach(category => {
 
-        const li = document.createElement("div");
+        const categoryDiv = document.createElement("div");
         
-        li.innerHTML = `
-        <a class="nav-link" href="#">${category.category_name}</a>
+        categoryDiv.innerHTML = `
+        <a onclick="loadCategoriesId(${category.category_id})" class="nav-link" href="#">${category.category_name}</a>
         `
-        categoriesContainer.appendChild(li)
+        categoriesContainer.appendChild(categoryDiv)
    });
 
 }
 
 
+const loadCategoriesId = async(CategoriesId) => {
+
+    const url = `https://openapi.programming-hero.com/api/news/category/0${CategoriesId}` 
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategoriesId(data.data);
+    // console.log(data.length);
+
+}
 
 
+const displayCategoriesId = (categoriesNews) =>{
+    console.log(categoriesNews);
+    const newsCardContainer = document.getElementById("news-card-container");
 
+    newsCardContainer.innerText = "";
+
+    categoriesNews.forEach(categoryNews => {
+        const divCategoryNews = document.createElement("div")
+        divCategoryNews.classList.add("card");
+        divCategoryNews.classList.add("mb-4");
+        divCategoryNews.innerHTML = `
+        <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="${categoryNews.thumbnail_url}" class="img-fluid rounded-start p-5" alt="...">
+                  </div>
+                  <div class="col-md-8 p-5">
+                    <div class="card-body">
+                      <h5 class="card-title">${categoryNews.title}</h5>
+                      <p class="card-text">${categoryNews.details.slice(0, 200) }</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+        `
+
+        newsCardContainer.appendChild(divCategoryNews)
+    });
+
+    
+    
+}
 
 
 
